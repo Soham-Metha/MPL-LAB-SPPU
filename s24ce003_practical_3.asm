be3db7674874d922340900acf344295a9f945da5
Soham Metha <sohammetha01@gmail.com>
2025-05-01 19:13:09 +0000

    02/05/2025 : 00:43:09

--- s24ce003_practical_3.asm
+++ s24ce003_practical_3.asm
@@ -40,7 +40,7 @@ _start:
 
     print   msg1, msg1_len
     MOV     RAX,  RBX                         ; load largest number in RAX
-    CALL    display_int
+    CALL    display_quad
     
     print msg2, msg2_len
 
@@ -48,7 +48,7 @@ _start:
     print_arr:
         print   crlf,   1
         MOV     RAX,    [RBP]                 ; load current number in RAX
-        CALL    display_int
+        CALL    display_quad
         ADD     RBP,    08H
         DEC     byte[cnt]
     JNZ print_arr
@@ -69,16 +69,4 @@ find_largest:
     continue:
         ADD RBP, 08H
         LOOP compare_all_numbers
-RET
-
-display_int:
-    MOV RDI, buffer                           ; destination for the ASCII values
-    MOV RCX, 10H                              ; how many times should we loop?
-
-    over_all_digits:
-        ROL RAX, 4H                           ; rotate the number by 4 bits so that the 'next MSB' is loaded into AL
-        hex_ascii_adjust                      ; macro for hex ascii adjust of AL
-    LOOP over_all_digits
-
-    print buffer, 10H                         ; print result
 RET
