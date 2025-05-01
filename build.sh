b24382b4be13ba960c8a8a989e8205961b125cab
Soham Metha <sohammetha01@gmail.com>
2025-05-01 15:40:33 +0000

    01/05/2025 : 21:10:33

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ nasm -f elf64 $prefix$1.asm &&
 ld $prefix$1.o -o $prefix$1.out &&
 
 #run executable
-./$prefix$1.out &&
+./$prefix$1.out
 
-#clean the trash
-rm $prefix$1.out $prefix$1.o
+# clean the trash
+# rm $prefix$1.out $prefix$1.o
