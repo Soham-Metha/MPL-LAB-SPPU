302cf2b0a40cf8ebabb6d8db09444dada2a81342
Soham Metha <sohammetha01@gmail.com>
2025-05-01 19:29:59 +0000

    02/05/2025 : 00:59:59

--- s24ce003_practical_7.asm
+++ s24ce003_practical_7.asm
@@ -45,11 +45,11 @@ section .bss
     gdtlimit:
         resw 1
     gdt:
-        resq 1     ; resd for a 32 bit pc
+        resd 1     ; resd for a 32 bit pc
     idtlimit:
         resw 1
     idt:
-        resq 1     ; resd for a 32 bit pc
+        resd 1     ; resd for a 32 bit pc
     ldt:
         resw 1
     tr:
