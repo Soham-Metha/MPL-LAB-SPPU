d6ae9bd216be9f3753b4b6303e7d4b54ed067217
Soham Metha <sohammetha01@gmail.com>
2025-05-01 22:55:04 +0000

    02/05/2025 : 04:25:04

--- s24ce003_practical_9helper.asm
+++ s24ce003_practical_9helper.asm
@@ -1,14 +1,26 @@
 %include 'macros.asm'
 
 section .data
-    extern charOccurMsg,charOccurMsgLen,scount,ncount,chacount
+    extern charOccurMsg,charOccurMsgLen,chacount
 
 section .bss
     pbuffer resb 10H
     extern buf_len,buffer
 
 section .text
-    global occr
+    global char,spaces,enters
+
+spaces:
+    mov bl,' '
+    CALL occr
+    
+enters:
+    mov bl,0x0A
+    CALL occr
+
+char:
+    mov bl,byte[cha]
+    CALL occr
 
 occr:
     print buffer,buf_len
@@ -22,5 +34,6 @@ occr:
     continue:
         inc     rsi
         loop    up3
-
+    MOV RAX,[chacount]
+    CALL display_quad
 ret
