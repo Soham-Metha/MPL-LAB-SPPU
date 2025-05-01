7649c85cb44cfd294e778d615544274658f7f99e
Soham Metha <sohammetha01@gmail.com>
2025-05-01 20:20:49 +0000

    02/05/2025 : 01:50:49

--- s24ce003_practical_6.asm
+++ s24ce003_practical_6.asm
@@ -29,10 +29,69 @@ section .bss
         resb 6
     output:
         resb 6
+    buffer:
+        resb 8
 
 section .text
 
+menustart:
 _start:
+    print menu_msg,menu_msg_len
+    read choice, 2
 
+    CMP byte[choice], '3'                           ; user has only 2 options, 1 & 2
+    JGE end                                         ; if user enters some other option, exit
 
-exit
+    CMP byte[choice], '0'                           ; 0 is not a valid choice
+    JE  end
+
+    print In_msg, in_msg_len
+    read input, 6
+
+    PUSH menustart                                  ; ret returns to the address at top of stack
+
+    cmp byte[choice], '1'
+    jz  h2bHandler
+
+    cmp byte[choice], '2'
+    jz  b2hHandler
+end:
+    exit
+
+h2bHandler:
+    get_bin_word
+    display_quad
+
+get_bin_word:
+    XOR RAX, RAX
+    MOV RSI, Input
+    MOV RCX, 4                                ; how many times should we loop?(digit count)
+
+    over_all_digits:
+        SHL RAX, 4                             ; rotate the number by 4 bits so that the 'next MSB' is loaded into AL
+        MOV BL, [RSI]
+        AND BL, 0FH
+
+        CMP BL, 09H
+        JLE not_alphabet
+        SUB BL, 07H
+
+        not_alphabet:
+            SUB BL, 30H
+            OR  AL, BL
+            INC RSI
+    LOOP over_all_digits
+
+RET
+
+display_quad:
+    MOV RDI, buffer                           ; destination for the ASCII values
+    MOV RCX, 10H                              ; how many times should we loop?
+
+    over_all_digits:
+        ROL RAX, 4H                           ; rotate the number by 4 bits so that the 'next MSB' is loaded into AL
+        hex_ascii_adjust                      ; macro for hex ascii adjust of AL
+    LOOP over_all_digits
+
+    print   buffer, 10H                         ; print result
+RET
