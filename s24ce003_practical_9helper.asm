03b7b872a5ef6466d689d0b84d9fb97dbd0b8d01
Soham Metha <sohammetha01@gmail.com>
2025-05-01 22:26:25 +0000

    02/05/2025 : 03:56:25

--- s24ce003_practical_9helper.asm
+++ s24ce003_practical_9helper.asm
@@ -13,7 +13,9 @@ section .text
 occ:
     print charOccurMsg,charOccurMsgLen
     mov rsi,buffer
+    print charOccurMsg,charOccurMsgLen
     mov RCX,qword[buf_len]
+    print charOccurMsg,charOccurMsgLen
     up3:
         mov al, byte[rsi]
         cmp al, bl
