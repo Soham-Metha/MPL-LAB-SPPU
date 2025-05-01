740fccfb5db752cfa4f08592c7deb788dffe1bf6
Soham Metha <sohammetha01@gmail.com>
2025-05-01 16:11:57 +0000

    01/05/2025 : 21:41:57

--- s24ce003_practical_4.asm
+++ s24ce003_practical_4.asm
@@ -1,21 +1,24 @@
 %include 'macros.asm'
 
 section .data
-menu_msg:
-    db  0x0A
-    db  "1. Addition        ",0x0A
-    db  "2. Subtraction     ",0x0A
-    db  "3. Multiplication  ",0x0A
-    db  "4. Division        ",0x0A
-    db  "5. Shift left      ",0x0A
-    db  "6. Shift right     ",0x0A
-    db  "Enter your choice : "
-menu_msg_len: equ $-menu_msg
-result_message      db  "Result : "
-result_message_len  equ $-result_message
+    menu_msg:
+        db  0x0A,"-------------------"0x0A
+        db  "1. Addition        ",0x0A
+        db  "2. Subtraction     ",0x0A
+        db  "3. Multiplication  ",0x0A
+        db  "4. Division        ",0x0A
+        db  "5. Shift left      ",0x0A
+        db  "6. Shift right     ",0x0A
+        db  "-------------------"0x0A
+        db  "Enter your choice : "
 
-num1  dq  20H
-num2  dq  10H
+    menu_msg_len: equ $-menu_msg
+
+    result_message      db  "Result : "
+    result_message_len  equ $-result_message
+
+    num1  dq  20H
+    num2  dq  10H
 
 section .bss
 
@@ -26,9 +29,6 @@ section .text
 
 menustart:
     CALL    display_int
-
-    read    buffer, 10H
-
     MOV qword[buffer], 0
 _start:
 
@@ -43,53 +43,53 @@ _start:
     PUSH menustart
 
     cmp byte[choice], '1'
-    jz  l1
+    jz  addHandler
 
     cmp byte[choice], '2'
-    jz  l2
+    jz  subHandler
 
     cmp byte[choice], '3'
-    jz  l3
+    jz  mulHandler
 
     cmp byte[choice], '4'
-    jz  l4
+    jz  divHandler
 
     cmp byte[choice], '5'
-    jz  l5
+    jz  shlHandler
 
     cmp byte[choice], '6'
-    jz  l6
+    jz  shrHandler
 
 end:
-exit
+    exit
 
-l1:
+addHandler:
     MOV RAX, [num1]
     ADD RAX, qword[num2]
 RET
 
-l2:
+subHandler:
     MOV RAX, [num1]
     SUB RAX, qword[num2]
 RET
 
-l3:
+mulHandler:
     MOV  RAX, [num1]
     IMUL qword[num2]
 RET
 
-l4:
+divHandler:
     MOV RDX, 0
     MOV RAX, [num1]
     IDIV qword[num2]
 RET
 
-l5:
+shlHandler:
     MOV RAX, [num1]
     SHL RAX, 1
 RET
 
-l6:
+shrHandler:
     MOV RAX, [num1]
     SHR RAX, 1
 RET
