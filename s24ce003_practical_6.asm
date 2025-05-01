b045f633a7eae504337bab0319fd3728707682b5
Soham Metha <sohammetha01@gmail.com>
2025-05-01 19:51:20 +0000

    02/05/2025 : 01:21:20

--- s24ce003_practical_6.asm
+++ s24ce003_practical_6.asm
@@ -2,9 +2,33 @@
 
 section .data
 
+    menu_msg:
+        db  0x0A,0x0A
+        db  "-------------------",0x0A
+        db "1. HEX to BCD       ",0x0A
+        db "2. BCD to HEX       ",0x0A
+        db "3. EXIT             ",0x0A
+        db  "-------------------",0x0A
+        db "Your Choice : "
+    
+    menu_msg_len: equ $-menu_msg
+    In_msg:
+        db "Enter Input: "
+    in_msg_len: equ $-In_msg
+
+    invalid_msg db "Invalid input!", 0xA
+    Out_msg:
+        db "Result: "
+    out_msg_len: equ $-Out_msg
 
 section .bss
 
+    choice:
+        resb 2
+    input:
+        resb 6
+    output:
+        resb 6
 
 section .text
 
