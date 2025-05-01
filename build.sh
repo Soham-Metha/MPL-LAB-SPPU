e156558fe739c1904779c7383f42f2683e24a67f
Soham Metha <sohammetha01@gmail.com>
2025-05-01 22:24:01 +0000

    02/05/2025 : 03:54:01

--- build.sh
+++ build.sh
@@ -4,13 +4,14 @@ prefix=s24ce003_practical_
 
 clear &&
 #build object file
-nasm -f elf64 $prefix$1.asm
 
 if [[ "$1" == "9" ]]; then
     echo "exp 9"
+    nasm -f elf64 s24ce003_practical_9.asm &&
     nasm -f elf64 s24ce003_practical_9helper.asm &&
     ld -o s24ce003_practical_9.out s24ce003_practical_9.o s24ce003_practical_9helper.o ;
 else
+    nasm -f elf64 $prefix$1.asm
     ld $prefix$1.o -o $prefix$1.out;
 fi
 #create executable
