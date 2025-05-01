7d27b6afdc45368cf43ac9842d5216925d9aed41
Soham Metha <sohammetha01@gmail.com>
2025-05-01 21:01:10 +0000

    02/05/2025 : 02:31:10

--- s24ce003_practical_6.asm
+++ s24ce003_practical_6.asm
@@ -92,7 +92,7 @@ hex_to_ascii_hex:
     MOV RCX, 10H                              ; how many times should we loop?
 
     over_all_digits:
-        ROL RAX, 4H                           ; rotate the number by 4 bits so that the 'next MSB' is loaded into AL
+        ROL AX, 4H                           ; rotate the number by 4 bits so that the 'next MSB' is loaded into AL
         hex_ascii_adjust                      ; macro for hex ascii adjust of AL
     LOOP over_all_digits
     print buffer,10H
