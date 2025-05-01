9477a60054d1c864f0a15f24d06d7affdbc6d64a
Soham Metha <sohammetha01@gmail.com>
2025-05-01 19:10:32 +0000

    02/05/2025 : 00:40:32

--- macros.asm
+++ macros.asm
@@ -59,3 +59,16 @@ section .data
 
 section .text
     global _start
+
+display_quad:
+    MOV RDI, buffer                           ; destination for the ASCII values
+    MOV RCX, 10H                              ; how many times should we loop?
+
+    over_all_digits:
+        ROL RAX, 4H                           ; rotate the number by 4 bits so that the 'next MSB' is loaded into AL
+        hex_ascii_adjust                      ; macro for hex ascii adjust of AL
+    LOOP over_all_digits
+
+    print   msg2,  msg2_len
+    print   buffer, 10H                         ; print result
+RET
