244e44653f3f42947a212a2d0ffbaabc72f4c400
Soham Metha <sohammetha01@gmail.com>
2025-05-01 19:10:58 +0000

    02/05/2025 : 00:40:58

--- s24ce003_practical_2.asm
+++ s24ce003_practical_2.asm
@@ -35,16 +35,3 @@ _start:
 exit
 
 ;-----------------------------------------------------------------------------------------------------------------------------
-
-display_quad:
-    MOV RDI, buffer                           ; destination for the ASCII values
-    MOV RCX, 10H                              ; how many times should we loop?
-
-    over_all_digits:
-        ROL RAX, 4H                           ; rotate the number by 4 bits so that the 'next MSB' is loaded into AL
-        hex_ascii_adjust                      ; macro for hex ascii adjust of AL
-    LOOP over_all_digits
-
-    print   msg2,  msg2_len
-    print   buffer, 10H                         ; print result
-RET
