049b6afd0246a1d7d159056367dcf109ef149b31
Soham Metha <sohammetha01@gmail.com>
2025-05-01 16:06:55 +0000

    01/05/2025 : 21:36:55

--- s24ce003_practical_4.asm
+++ s24ce003_practical_4.asm
@@ -1,22 +1,15 @@
 %include 'macros.asm'
 
 section .data
-add_msg             db  "1. Addition"
-add_msg_len         equ $-add_msg
-sub_msg             db  "2. Subtraction"
-sub_msg_len         equ $-sub_msg
-mul_msg             db  "3. Multiplication"
-mul_msg_len         equ $-mul_msg
-div_msg             db  "4. Division"
-div_msg_len         equ $-div_msg
-shl_msg             db  "5. Shift left"
-shl_msg_len         equ $-shl_msg
-shr_msg             db  "6. Shift right"
-shr_msg_len         equ $-shr_msg
-ask_choice_msg      db  "Enter your choice : "
-ask_choice_msg_len  equ $-ask_choice_msg
-disp_choice_msg     db  "You Chose : "
-disp_choice_msg_len equ $-disp_choice_msg
+menu_msg:
+    db  "1. Addition        ",0x0A
+    db  "2. Subtraction     ",0x0A
+    db  "3. Multiplication  ",0x0A
+    db  "4. Division        ",0x0A
+    db  "5. Shift left      ",0x0A
+    db  "6. Shift right     ",0x0A
+    db  "Enter your choice : "
+menu_msg_len: equ $-menu_msg
 result_message      db  "Result : "
 result_message_len  equ $-result_message
 
@@ -27,121 +20,87 @@ section .bss
 
     choice resb 1
     buffer resb 10H
-    bitcnt resq 1
 
 section .text
 
 menustart:
     CALL    display_int
-    printbr
-    read    buffer, 10H
-    read    buffer, 10H
 
-    clear_screen
+    read    buffer, 10H
 
     MOV qword[buffer], 0
 _start:
 
-    printbr
-    println add_msg, add_msg_len
-    println sub_msg, sub_msg_len
-    println mul_msg, mul_msg_len
-    println div_msg, div_msg_len
-    println shl_msg, shl_msg_len
-    println shr_msg, shr_msg_len
-    printbr
+    print   menu_msg, menu_msg_len
+    read    choice,   1
 
-    print   ask_choice_msg, ask_choice_msg_len
-    read    choice,         1
-    printbr
+    CMP byte[choice], '7'
+    JGE end
 
-    SUB     byte[choice],    '0'
-    print   disp_choice_msg, disp_choice_msg_len
-    MOV     RAX,             [choice]
-    CALL    display_int
+    print result_message, result_message_len
 
-    cmp byte[choice], 1
+    PUSH menustart
+
+    cmp byte[choice], '1'
     jz  l1
 
-    cmp byte[choice], 2
+    cmp byte[choice], '2'
     jz  l2
 
-    cmp byte[choice], 3
+    cmp byte[choice], '3'
     jz  l3
 
-    cmp byte[choice], 4
+    cmp byte[choice], '4'
     jz  l4
 
-    cmp byte[choice], 5
+    cmp byte[choice], '5'
     jz  l5
 
-    cmp byte[choice], 6
+    cmp byte[choice], '6'
     jz  l6
 
+end:
 exit
 
 l1:
-    print   crlf, 1
-    println add_msg, add_msg_len
-    printbr result_message, result_message_len
     MOV RAX, [num1]
     ADD RAX, qword[num2]
-    
-jmp menustart
+RET
 
 l2:
-    print crlf, 1
-    println sub_msg, sub_msg_len
-    printbr result_message, result_message_len
     MOV RAX, [num1]
     SUB RAX, qword[num2]
-jmp menustart
+RET
 
 l3:
-    print crlf, 1
-    println mul_msg, mul_msg_len
-    printbr result_message, result_message_len
     MOV  RAX, [num1]
     IMUL qword[num2]
-jmp menustart
+RET
 
 l4:
-    print crlf, 1
-    println div_msg, div_msg_len
-    printbr result_message, result_message_len
     MOV RDX, 0
     MOV RAX, [num1]
     IDIV qword[num2]
-jmp menustart
+RET
 
 l5:
-    print crlf, 1
-    println shl_msg, shl_msg_len
-    printbr
-
-    print result_message, result_message_len
     MOV RAX, [num1]
-    SHL RAx, 1
-jmp menustart
+    SHL RAX, 1
+RET
 
 l6:
-    print crlf, 1
-    println shr_msg, shr_msg_len
-    printbr
-
-    print result_message, result_message_len
     MOV RAX, [num1]
-    SHR RAx, 1
-jmp menustart
+    SHR RAX, 1
+RET
 
 display_int:
     MOV RDI, buffer                           ; destination for the ASCII values
-    MOV RCX, [qword_digit_count]              ; how many times should we loop?
+    MOV RCX, 10H                              ; how many times should we loop?
 
     over_all_digits:
         ROL RAX, 4H                           ; rotate the number by 4 bits so that the 'next MSB' is loaded into AL
         hex_ascii_adjust                      ; macro for hex ascii adjust of AL
     LOOP over_all_digits
 
-    printtr buffer, [qword_digit_count]       ; trim the leading '0's from buffer, and print result
+    print buffer, 10H                         ; print result
 RET
