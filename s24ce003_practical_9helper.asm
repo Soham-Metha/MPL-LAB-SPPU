9b7782e40a9a1fe873a9787ad1e5a3cc86dd0f10
Soham Metha <sohammetha01@gmail.com>
2025-05-01 22:43:43 +0000

    02/05/2025 : 04:13:43

--- s24ce003_practical_9helper.asm
+++ s24ce003_practical_9helper.asm
@@ -11,14 +11,12 @@ section .text
     global occ
 
 occ:
-    print charOccurMsg,charOccurMsgLen
     mov rsi,buffer
     mov RCX,qword[buf_len]
     up3:
         mov al, byte[rsi]
         cmp al, bl
         jne continue_up_occ
-    print charOccurMsg,charOccurMsgLen
         inc byte[chacount]
     continue_up_occ:
         inc     rsi
