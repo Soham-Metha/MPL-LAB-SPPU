3205fee948e7a4d5c9295e7b6647389bde453efd
Soham Metha <sohammetha01@gmail.com>
2025-05-01 22:50:01 +0000

    02/05/2025 : 04:20:01

--- s24ce003_practical_9helper.asm
+++ s24ce003_practical_9helper.asm
@@ -8,18 +8,18 @@ section .bss
     extern buf_len,buffer
 
 section .text
-    global occ
+    global occr
 
-occ:
+occr:
     print buffer,buf_len
     mov rsi,buffer
     mov RCX,qword[buf_len]
     up3:
         mov al, byte[rsi]
         cmp al, bl
-        jne continue_up_occ
+        jne continue
         inc byte[chacount]
-    continue_up_occ:
+    continue:
         inc     rsi
         loop    up3
 
