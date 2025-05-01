ec0a29f78fac714555a1a67e5ef87aeadc7758f0
Soham Metha <sohammetha01@gmail.com>
2025-05-01 23:09:19 +0000

    02/05/2025 : 04:39:19

--- s24ce003_practical_9helper.asm
+++ s24ce003_practical_9helper.asm
@@ -65,12 +65,12 @@ ret
 
 display_word:
     MOV RDI, pbuffer                           ; destination for the ASCII values
-    MOV RCX, 08H                              ; how many times should we loop?
+    MOV RCX, 04H                              ; how many times should we loop?
 
     over_all_digits:
         ROL AX, 4H                           ; rotate the number by 4 bits so that the 'next MSB' is loaded into AL
         hex_ascii_adjust                      ; macro for hex ascii adjust of AL
     LOOP over_all_digits
 
-    print pbuffer, 08H                         ; print result
+    print pbuffer, 04H                         ; print result
 RET
