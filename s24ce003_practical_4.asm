77e0db88415a6ab6c304b326fd35fcdb4b4e27d4
Soham Metha <sohammetha01@gmail.com>
2025-05-01 16:14:22 +0000

    01/05/2025 : 21:44:22

--- s24ce003_practical_4.asm
+++ s24ce003_practical_4.asm
@@ -2,7 +2,7 @@
 
 section .data
     menu_msg:
-        db  0x0A,
+        db  0x0A,0x0A
         db  "-------------------",0x0A
         db  "1. Addition        ",0x0A
         db  "2. Subtraction     ",0x0A
